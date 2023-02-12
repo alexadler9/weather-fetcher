1f96fb031da8f4d03ee3439d0456106ca63e796b
Alexandra <orlova@stroypribor.ru>
2023-02-12 15:22:11 +0500

    - added dependency injection support

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -3,31 +3,20 @@ package com.example.weatherfetcher
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.weatherfetcher.feature.weather_screen.WeatherInteractor
-import com.example.weatherfetcher.feature.weather_screen.data.WeatherApiClient
-import com.example.weatherfetcher.feature.weather_screen.data.WeatherRemoteSource
-import com.example.weatherfetcher.feature.weather_screen.data.WeatherRepoImpl
 import com.example.weatherfetcher.feature.weather_screen.ui.WeatherScreenPresenter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var presenter: WeatherScreenPresenter
+    private val presenter: WeatherScreenPresenter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        presenter = WeatherScreenPresenter(
-            WeatherInteractor(
-                WeatherRepoImpl(
-                    WeatherRemoteSource(WeatherApiClient.getApi())
-                )
-            )
-        )
 
         val tvWeather = findViewById<TextView>(R.id.tvWeather)
 
