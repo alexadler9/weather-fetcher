d89227035324fe1be472abac166813870312b5d1
alexadler9 <alexadler@bk.ru>
2023-12-15 14:30:07 +0500

    Added view binding

--- app/src/main/java/ru/alexadler9/weatherfetcher/MainActivity.kt
+++ app/src/main/java/ru/alexadler9/weatherfetcher/MainActivity.kt
@@ -1,12 +1,10 @@
 package ru.alexadler9.weatherfetcher
 
 import android.os.Bundle
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.alexadler9.weatherfetcher.databinding.ActivityMainBinding
 import ru.alexadler9.weatherfetcher.feature.weatherscreen.ui.UiEvent
 import ru.alexadler9.weatherfetcher.feature.weatherscreen.ui.ViewState
 import ru.alexadler9.weatherfetcher.feature.weatherscreen.ui.WeatherScreenViewModel
@@ -15,23 +13,25 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: WeatherScreenViewModel by viewModel()
 
-    private val pbWeather: ProgressBar by lazy { findViewById(R.id.pbWeather) }
-    private val tvWeather: TextView by lazy { findViewById(R.id.tvWeather) }
-    private val fabWeather: FloatingActionButton by lazy { findViewById(R.id.fabWeatherFetch) }
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.viewState.observe(this, ::render)
 
-        fabWeather.setOnClickListener {
+        binding.fabWeatherFetch.setOnClickListener {
             viewModel.processUiEvent(UiEvent.OnButtonClicked)
         }
     }
 
     private fun render(viewState: ViewState) {
-        pbWeather.isVisible = viewState.isLoading
-        tvWeather.text = viewState.temperature
+        with(binding) {
+            pbWeather.isVisible = viewState.isLoading
+            tvWeather.text = viewState.temperature
+        }
     }
 }
