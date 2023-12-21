c137c51f5a8250c5a9391a10248495c04ab5ef6a
alexadler9 <alexadler@bk.ru>
2023-12-22 03:27:09 +0500

    Added unit tests for mappers

---
+++ app/src/test/java/ru/alexadler9/weatherfetcher/feature/weatherscreen/data/MapperKtTest.kt
@@ -0,0 +1,70 @@
+package ru.alexadler9.weatherfetcher.feature.weatherscreen.data
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.greaterThanOrEqualTo
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import ru.alexadler9.weatherfetcher.feature.weatherscreen.data.remote.model.ForecastCityRemoteModel
+import ru.alexadler9.weatherfetcher.feature.weatherscreen.data.remote.model.ForecastRemoteModel
+import ru.alexadler9.weatherfetcher.feature.weatherscreen.data.remote.model.ForecastWeatherRemoteModel
+import ru.alexadler9.weatherfetcher.utility.FORECAST_WEATHER_REMOTE_MODEL
+import ru.alexadler9.weatherfetcher.utility.GEO_REMOTE_MODEL
+import ru.alexadler9.weatherfetcher.utility.WEATHER_REMOTE_MODEL
+
+class MapperKtTest {
+
+    @Test
+    fun `converts remote weather model into a local`() {
+        val weatherModel = WEATHER_REMOTE_MODEL.toDomain()
+
+        assertEquals(weatherModel.datetime, "01.01.2024 12:00")
+        assertEquals(weatherModel.temp, WEATHER_REMOTE_MODEL.main.temp)
+        assertEquals(weatherModel.tempFeelsLike, WEATHER_REMOTE_MODEL.main.tempFeelsLike)
+        assertEquals(weatherModel.humidity, WEATHER_REMOTE_MODEL.main.humidity)
+        assertEquals(weatherModel.windSpeed, WEATHER_REMOTE_MODEL.wind.speed)
+        assertThat(weatherModel.details.size, greaterThanOrEqualTo(1))
+        assertEquals(weatherModel.details.size, WEATHER_REMOTE_MODEL.details.size)
+        assertEquals(weatherModel.details.first().id, WEATHER_REMOTE_MODEL.details.first().id)
+        assertEquals(weatherModel.details.first().main, WEATHER_REMOTE_MODEL.details.first().main)
+        assertEquals(
+            weatherModel.details.first().description,
+            WEATHER_REMOTE_MODEL.details.first().description
+        )
+        assertEquals(weatherModel.details.first().icon, WEATHER_REMOTE_MODEL.details.first().icon)
+    }
+
+    @Test
+    fun `converts forecast-every-three-hours into a forecast-daily for the next three days`() {
+        val listForecastWeatherRemoteModel = mutableListOf<ForecastWeatherRemoteModel>()
+        for (i in 0..40) {
+            // Change dt parameter with 3-hour step
+            // As a result, we get a 5-day forecast including weather data with 3-hour step
+            listForecastWeatherRemoteModel.add(
+                FORECAST_WEATHER_REMOTE_MODEL.copy(dt = FORECAST_WEATHER_REMOTE_MODEL.dt + i * 10800)
+            )
+        }
+
+        val forecastRemoteModel = ForecastRemoteModel(
+            cnt = 40,
+            city = ForecastCityRemoteModel(timezone = 0),
+            weather = listForecastWeatherRemoteModel
+        )
+
+        val forecastModel = forecastRemoteModel.toDomain()
+
+        assertEquals(forecastModel.weather.size, 3)
+        assertEquals(forecastModel.weather[0].date, "2 янв.")
+        assertEquals(forecastModel.weather[1].date, "3 янв.")
+        assertEquals(forecastModel.weather[2].date, "4 янв.")
+    }
+
+    @Test
+    fun `converts remote geo model into a local`() {
+        val geoModel = GEO_REMOTE_MODEL.toDomain()
+
+        assertEquals(geoModel.latitude, GEO_REMOTE_MODEL.lat)
+        assertEquals(geoModel.longitude, GEO_REMOTE_MODEL.lon)
+        assertEquals(geoModel.country, GEO_REMOTE_MODEL.country)
+        assertEquals(geoModel.localNames, GEO_REMOTE_MODEL.localNames)
+    }
+}
--- app/src/test/java/ru/alexadler9/weatherfetcher/utility/Constants.kt
+++ app/src/test/java/ru/alexadler9/weatherfetcher/utility/Constants.kt
@@ -1,10 +1,11 @@
 package ru.alexadler9.weatherfetcher.utility
 
+import ru.alexadler9.weatherfetcher.feature.weatherscreen.data.remote.model.*
 import ru.alexadler9.weatherfetcher.feature.weatherscreen.domain.model.*
 
-val CITY_NAME_INCORRECT = "123456"
-val CITY_NAME_1 = "Москва"
-val CITY_NAME_2 = "Челябинск"
+const val CITY_NAME_INCORRECT = "123456"
+const val CITY_NAME_1 = "Москва"
+const val CITY_NAME_2 = "Челябинск"
 
 val GEO_MODEL_1 = GeoModel(
     latitude = 55.761665,
@@ -12,6 +13,7 @@ val GEO_MODEL_1 = GeoModel(
     country = "RU",
     localNames = mapOf("ru" to CITY_NAME_1)
 )
+
 val GEO_MODEL_2 = GeoModel(
     latitude = 61.429722,
     longitude = 55.154442,
@@ -54,4 +56,53 @@ val FORECAST_MODEL = ForecastModel(
             )
         )
     )
+)
+
+val WEATHER_REMOTE_MODEL = WeatherRemoteModel(
+    dt = 1704110400,
+    timezone = 0,
+    main = WeatherMainRemoteModel(
+        temp = "20.00",
+        tempFeelsLike = "18.75",
+        humidity = "23"
+    ),
+    wind = WeatherWindRemoteModel(
+        speed = "2"
+    ),
+    details = listOf(
+        WeatherDetailsRemoteModel(
+            id = 0,
+            main = "Ясно",
+            description = "Ясно",
+            icon = "01"
+        )
+    )
+)
+
+val FORECAST_WEATHER_REMOTE_MODEL = ForecastWeatherRemoteModel(
+    dt = 1704110400,
+    main = WeatherMainRemoteModel(
+        temp = "20.00",
+        tempFeelsLike = "18.75",
+        humidity = "23"
+    ),
+    wind = WeatherWindRemoteModel(
+        speed = "2"
+    ),
+    details = listOf(
+        WeatherDetailsRemoteModel(
+            id = 0,
+            main = "Ясно",
+            description = "Ясно",
+            icon = "01"
+        )
+    )
+)
+
+val GEO_REMOTE_MODEL = GeoRemoteModel(
+    name = "Москва",
+    localNames = mapOf("ru" to "Москва"),
+    lat = 55.761665,
+    lon = 37.606667,
+    country = "RU"
 )
