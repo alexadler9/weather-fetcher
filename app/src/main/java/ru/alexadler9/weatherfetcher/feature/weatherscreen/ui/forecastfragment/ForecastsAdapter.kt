7f1a69115d255add4b185d083c25bbdffaa31437
alexadler9 <alexadler@bk.ru>
2023-12-27 02:17:40 +0500

    Added view binding in forecast adapter

--- app/src/main/java/ru/alexadler9/weatherfetcher/feature/weatherscreen/ui/forecastfragment/ForecastsAdapter.kt
+++ app/src/main/java/ru/alexadler9/weatherfetcher/feature/weatherscreen/ui/forecastfragment/ForecastsAdapter.kt
@@ -2,10 +2,9 @@ package ru.alexadler9.weatherfetcher.feature.weatherscreen.ui.forecastfragment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import ru.alexadler9.weatherfetcher.R
+import ru.alexadler9.weatherfetcher.databinding.ItemForecastBinding
 import ru.alexadler9.weatherfetcher.feature.weatherscreen.domain.model.ForecastWeatherModel
 
 class ForecastsAdapter : RecyclerView.Adapter<ForecastsAdapter.ForecastViewHolder>() {
@@ -16,31 +15,29 @@ class ForecastsAdapter : RecyclerView.Adapter<ForecastsAdapter.ForecastViewHolde
             notifyDataSetChanged()
         }
 
-    class ForecastViewHolder(val rootView: CardView) : RecyclerView.ViewHolder(rootView) {
-
-        val tvDayOfWeek = rootView.findViewById<TextView>(R.id.tvDayOfWeek)
-        val tvDate = rootView.findViewById<TextView>(R.id.tvDate)
-        val tvDayTemp = rootView.findViewById<TextView>(R.id.tvDayTemp)
-        val tvNightTemp = rootView.findViewById<TextView>(R.id.tvNightTemp)
+    class ForecastViewHolder(private val binding: ItemForecastBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: ForecastWeatherModel) {
-            tvDayOfWeek.text = item.dayOfWeek.uppercase()
-            tvDate.text = item.date
-            tvDayTemp.text = rootView.context.getString(
-                R.string.temperature_value,
-                item.tempDay.toFloat()
-            ).replace(',', '.')
-            tvNightTemp.text = rootView.context.getString(
-                R.string.temperature_value,
-                item.tempNight.toFloat()
-            ).replace(',', '.')
+            with(binding) {
+                tvDayOfWeek.text = item.dayOfWeek.uppercase()
+                tvDate.text = item.date
+                tvDayTemp.text = root.context.getString(
+                    R.string.temperature_value,
+                    item.tempDay.toFloat()
+                ).replace(',', '.')
+                tvNightTemp.text = root.context.getString(
+                    R.string.temperature_value,
+                    item.tempNight.toFloat()
+                ).replace(',', '.')
+            }
         }
 
         companion object {
             fun inflateFrom(parent: ViewGroup): ForecastViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.forecast_item, parent, false) as CardView
-                return ForecastViewHolder(view)
+                val binding = ItemForecastBinding.inflate(layoutInflater, parent, false)
+                return ForecastViewHolder(binding)
             }
         }
     }
